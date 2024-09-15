ff223ea25aaab78dd33bc2519b037a9b07c86138
Asmita Dahule <dahuleasmita@gmail.com>
2024-09-15 11:53:10 +0530

    roteted aaray

--- binary search/AgnosticBS.java
+++ binary search/AgnosticBS.java
@@ -1,2 +1,44 @@
-public class AgnosticBS {
+class RotetedArray {
+    public static void main(String[] args) {
+        int[] arr = { 10, 14, 15, 20, 2, 4, 8, 9 };
+        int target = 15;
+        int ans = search(arr, target);
+        System.out.println(ans);
+
+    }
+
+    static int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int pivot = 0;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                 pivot = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return bsearch(arr, target, pivot);
+
+    }
+
+    static int bsearch(int[] arr, int target, int pivot) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
 }
--- binary search/MountainArray.java
+++ binary search/MountainArray.java
@@ -1,6 +1,6 @@
 class MountainArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,5,4,3,2,1,-2,-6};
+        int[] arr = {1,2,3,4,5,3,2,1,-2,-6};
         int ans = peakIndexInMountainArray(arr);
         System.out.println(ans);
     }
