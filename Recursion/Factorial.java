d7942432d18a758b88453302cf077ad0eec7762d
Asmita Dahule <dahuleasmita@gmail.com>
2024-10-29 09:15:45 +0530

    new file

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,3 +1,16 @@
 public class Factorial {
-    
+    public static void main(String[] args) {
+        int n=5;
+        int ans =factorial(n);
+        System.out.println(ans);
+    }
+    public static int factorial(int n)
+    {
+        if(n == 0 || n == 1)
+        return 1;
+
+        int fact = factorial(n-1);
+        int fact_n= n * fact;
+        return fact_n;
+    }
 }
