d619361bf296183d76332bcea4c8741b5b95d58c
Asmita Dahule <dahuleasmita@gmail.com>
2025-02-06 07:39:35 +0530

    complement

---
+++ striver's playlist/bit manipulation/EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+
+    public static void main(String[] args) {
+        int num = 18;
+        if((num & 1) == 1)
+            System.out.println("odd");
+        else 
+            System.out.println("even");
+     } 
+ }
--- striver's playlist/bit manipulation/TwosComp.java
+++ striver's playlist/bit manipulation/TwosComp.java
@@ -1,27 +1,56 @@
 public class TwosComp {
+    // public static void main(String[] args) {
+    //     int[] num = new int[]{1,0,0,1,0,1,0};
+    //     System.out.println(convert(num));
+        // int cnt = 0;
+        // for(int i = num.length-1; i >= 0; i--) {
+        //     if(num[i] == 0) { 
+        //         cnt++;
+        //         num[i] = 0;
+        //     }
+        //     else {
+        //         num[i] = 1;
+        //         cnt++;
+        //         break;
+        //     }
+        // } 
+
+        // for(int i = num.length - cnt - 1; i >= 0; i--) {
+        //     if(num[i] == 1) num[i] = 0;
+        //     else num[i] = 1;
+        // }
+        // StringBuilder comp = new StringBuilder();
+        // for(int n : num) {
+        //     comp.append(n);
+        // } 
+        // System.out.println(comp.toString());
+    //}
+
     public static void main(String[] args) {
         int[] num = new int[]{1,0,0,1,0,1,0};
-        int cnt = 0;
+        System.out.println(convert(num));
+    }
+
+    public static String convert(int[] num) {
+        // one's complement
+        for(int i = 0; i < num.length; i++) {
+            num[i] = (num[i] == 1) ? 0 : 1;
+        }
+
+
         for(int i = num.length-1; i >= 0; i--) {
-            if(num[i] == 0) { 
-                cnt++;
-                num[i] = 0;
-            }
-            else {
+            if(num[i] == 0) {
                 num[i] = 1;
-                cnt++;
                 break;
+            } else {
+                num[i] = 0;
             }
         } 
 
-        for(int i = num.length - cnt - 1; i >= 0; i--) {
-            if(num[i] == 1) num[i] = 0;
-            else num[i] = 1;
-        }
-        StringBuilder comp = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         for(int n : num) {
-            comp.append(n);
-        } 
-        System.out.println(comp.toString());
+            res.append(n);
+        }
+        return res.toString();
     }
 }
