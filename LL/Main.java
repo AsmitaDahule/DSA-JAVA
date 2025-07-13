e397b34544e784c3386c6b7a461e7d7b0375949f
Asmita Dahule <dahuleasmita@gmail.com>
2025-07-13 07:39:11 +0530

    java basic

--- LL/Main.java
+++ LL/Main.java
@@ -41,6 +41,27 @@ class LL {
     size += 1;
 }
 
+
+public void nthposition(int value, int index){
+    if(index == 0){
+        insertFirst(value);
+    } 
+    if(index == size){
+        insertLast(value);
+    } 
+
+    Node temp = head;
+    for(int i = 1; i < index; i++){
+        temp = temp.next;
+    }
+    Node node = new Node(value);
+
+    node.next = temp.next;
+    temp.next = node;
+
+
+}
+
     public void display(){
         Node temp = head;
         if(head == null) {
@@ -56,21 +77,35 @@ class LL {
         System.out.println(size);
 
     }
+
+    public int deleteFirst(){
+        int val = head.value;
+        head = head.next;
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        return val;
+    }
 }
 
 public class Main{
     public static void main(String[] args) {
         LL list = new LL();
-        list.insertFirst(5);
-        list.insertFirst(45);
-        list.insertFirst(55);
-        list.insertFirst(35);
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
 
         list.display();
 
         list.insertLast(9);
 
         list.display();
-       
+
+        list.nthposition(99, 3);
+        list.display();
+        list.deleteFirst();
+        list.display();
     }
 }
---
+++ striver's playlist/java basic/Basic.java
@@ -0,0 +1,9 @@
+class Basic {
+  public static void main(String[] args) {
+    String str = "123";
+    // int x = Integer.parseInt(str);
+    System.out.println(5+str);
+    System.out.println(Integer.parseInt(str));
+
+  }
+}
